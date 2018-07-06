3da7b177285268518ac871da858ec59519771635
j_tada <tada@people.co.jp>
2018-07-06 17:31:38 +0900

    GuestAdditions 入って共有がうまくいくように、カーネルバージョンを変えるようにした

--- packer/scripts/cleanup.sh
+++ packer/scripts/cleanup.sh
@@ -3,5 +3,5 @@ yum -y clean all
 rm -rf VBoxGuestAdditions_*.iso
 rm -rf /tmp/rubygems-*
 
-# Vagrant が共有ディレクトリマウントに失敗しないようになる
-yum -y update
+# Vagrant GuestAdditions が動くためのカーネル
+yum -y install http://vault.centos.org/6.4/cr/x86_64/Packages/kernel-devel-2.6.32-431.el6.x86_64.rpm
